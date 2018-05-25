20992e0cde7a706d152105bf5535b9375e991827
Markus Busche <m.busche@medisoftware.de>
2018-05-25 09:15:58 +0200

    Initial commit

---
+++ copypathmenu/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.martinstoeckli.ch/copypathmenu/copypathmenu_4_0.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+
+  softwareName  = 'CopyPathMenu'
+
+  checksum      = '294A1991C5E4713B438C971AC53EECFA'
+  checksumType  = 'md5'
+  
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
