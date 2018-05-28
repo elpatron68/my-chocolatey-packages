ae80613b7f245b08e460cf22227a96579b590bc2
Markus Busche <m.busche@medisoftware.de>
2018-05-28 19:03:49 +0200

    LANconfig

---
+++ LANconfig/tools/chocolateyinstall.ps1
@@ -0,0 +1,39 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.lancom-systems.de/fileadmin/download/LANtools/LANconfig-10.12.0091-RU7.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  
+  softwareName  = 'LANconfig*'
+
+  checksum      = '5D78EBE35FF0CF58CC901B860DF380E6BA133B95D029B808503E673DEC011AB7'
+  checksumType  = 'sha256'
+  
+  silentArgs    = "/params:/silent"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
