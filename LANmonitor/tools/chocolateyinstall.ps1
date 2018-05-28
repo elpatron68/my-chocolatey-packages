6c57d9b672c9b2062055271b4f98fd0dcae24e01
Markus Busche <m.busche@medisoftware.de>
2018-05-28 19:19:32 +0200

    LANconfig, LANmonitor

--- LANmonitor/tools/chocolateyinstall.ps1
+++ LANmonitor/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.lancom-systems.de/fileadmin/download/LANtools/LANconfig-10.12.0091-RU7.exe'
+$url        = 'https://www.lancom-systems.de/fileadmin/download/LANtools/LANmonitor-10.12.0030-RU7.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  
-  softwareName  = 'LANconfig*'
 
-  checksum      = '5D78EBE35FF0CF58CC901B860DF380E6BA133B95D029B808503E673DEC011AB7'
+  softwareName  = 'LANmonitor*'
+
+  checksum      = 'D66A662ED8C059C9CCF20C87DDBC75F0F0E5C2585764535DCA6C66452929C136'
   checksumType  = 'sha256'
   
   silentArgs    = "/params:/silent"
