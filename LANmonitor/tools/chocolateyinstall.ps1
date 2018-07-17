ef256c729201ea797e59af07e44850a81fb65fa8
Markus Busche <m.busche@medisoftware.de>
2018-07-17 16:11:27 +0200

    USBDLM

--- LANmonitor/tools/chocolateyinstall.ps1
+++ LANmonitor/tools/chocolateyinstall.ps1
@@ -12,7 +12,7 @@ $packageArgs = @{
 
   checksum      = 'D66A662ED8C059C9CCF20C87DDBC75F0F0E5C2585764535DCA6C66452929C136'
   checksumType  = 'sha256'
-  
+
   silentArgs    = "/params:/silent"
   validExitCodes= @(0)
 }
---
+++ usbdlm/tools/chocolateyinstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.uwe-sieber.de/files/usbdlm.msi'
+$url64      = 'https://www.uwe-sieber.de/files/usbdlm_x64.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'usbdlm*'
+
+  checksum      = '252251CED368E9D68D4A6D4DA3ACAFE206BB0609502E8A4AAB271BCB2099B59E'
+  checksumType  = 'sha256'
+  checksum64    = 'B791F48DFF2E29764545DD91D1D41F5A8E85F81C5FD58C270DD4052D16A72D3F'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
