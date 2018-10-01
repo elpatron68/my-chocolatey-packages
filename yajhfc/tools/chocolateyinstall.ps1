39fc1cfcdf8c164b1f32703b5b52a66c7d928c25
Markus Busche <m.busche@medisoftware.de>
2018-10-01 12:13:13 +0200

    LANconfig update

--- yajhfc/tools/chocolateyinstall.ps1
+++ yajhfc/tools/chocolateyinstall.ps1
@@ -13,7 +13,7 @@ $packageArgs = @{
   checksum      = '0636784A1CFAE7FE78565DF8A4A8D5463FE19004A82ABF75B35FE6E6C7B19DF1'
   checksumType  = 'sha256'
 
-  silentArgs    = "/VERYSILENT /NORESTART"
+  silentArgs    = "/SILENT /NORESTART"
   validExitCodes= @(0)
 }
 
