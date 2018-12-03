7a7573d20e3d2612a735a3b3fe3adfea60122f34
Markus Busche <m.busche@medisoftware.de>
2018-12-03 13:02:05 +0100

    rebootblocker

---
+++ rebootblocker/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.udse.de/download/RebootBlockerSetup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'rebootblocker*'
+
+  checksum      = '0AC4C5E204C9AD74233AA183F0FBE6E24605A8804CFC0EB17DEAE31A92D4BA02'
+  checksumType  = 'sha256'
+  
+  silentArgs    = "/silent /norestart"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
