2010c17dc73c88c09a32ace40fa7c71aebc541d9
Markus Busche <m.busche@medisoftware.de>
2019-02-20 11:05:23 +0100

    USBDLM 5.4.2

--- usbdlm/tools/chocolateyinstall.ps1
+++ usbdlm/tools/chocolateyinstall.ps1
@@ -13,9 +13,9 @@ $packageArgs = @{
 
   softwareName  = 'usbdlm*'
 
-  checksum      = '43C6F406AC2D0D57D13C53D3EC84D106452934E3BAA87E7C3B4AF13DC6699325'
+  checksum      = '486C9023B40DD46F99B1764947E6973DB8F125A8A91EE62DC1E99FAC7A4D37B0'
   checksumType  = 'sha256'
-  checksum64    = '19518ED14C4C695A4B24D5E09D88E780227980991EF983D9DC053EDAB7819DE3'
+  checksum64    = 'D37DE00F2FDED9985A4ACD881B72F9F7AEB578C08526693EDDF97B18CF0BE9A6'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
