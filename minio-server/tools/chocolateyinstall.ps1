ab58a31e99deb56d1836e88838fe845e9d4aa6ae
elpatron68 <elpatron@cepheus.uberspace.de>
2019-03-04 15:29:18 +0100

    removed unneccessary files

--- minio-server/tools/chocolateyinstall.ps1
+++ minio-server/tools/chocolateyinstall.ps1
@@ -1,18 +1,20 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl.minio.io/server/minio/release/windows-amd64/minio.exe'
+$url64      = 'https://dl.minio.io/server/minio/release/windows-amd64/minio.exe'
+$url        = 'https://dl.minio.io/server/minio/release/windows-386/minio.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
+  url64bit      = $url64
   FileFullPath  = "$toolsDir\minio.exe"
   softwareName  = 'minio-server*'
 
-  checksum      = '84F4298D9B5ED097F06359A890291E597B9C02C8687F3EF19AEC5C5EC7AA0034'
+  checksum      = '4AB74F1094DD315C316315C3C121CA03A10BCA1F9E76F0058651CBD831665B17'
   checksumType  = 'sha256'
-
-  
+  checksum64    = '84F4298D9B5ED097F06359A890291E597B9C02C8687F3EF19AEC5C5EC7AA0034'
+  checksumType64= 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
