995f63792578ed4ee1748176b6944713ecae7a88
Markus Busche <markus@medisoft.local>
2019-04-11 14:35:29 +0200

    minio server 2019.04.09

--- minio-server/tools/chocolateyinstall.ps1
+++ minio-server/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   FileFullPath    = "$toolsDir\minio.exe"
   softwareName    = 'minio-server*'
 
-  checksum64      = '6562411D01C21A11D53EBA71332CBA3335C8EAE9CB8277D891ABF7DF9FE88371'
+  checksum64      = '79291c36575b3eb4fda4480430624900ac6147461423ae094e3a1c59e5c52207'
   checksumType64  = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
