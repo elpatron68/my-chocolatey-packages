18fe1f79b94c721fe20c333a6e71b8edc3de9df6
elpatron68 <elpatron@cepheus.uberspace.de>
2019-04-14 15:21:09 +0200

    RaiDrive draft

---
+++ RaiDrive/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.raidrive.com/download.latest.php'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'RaiDrive*'
+  checksum      = '628F3A5D40D6EEDE0526585CB7E8940E695A0702AF2C9C62D9988FF46174C4D4'
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
