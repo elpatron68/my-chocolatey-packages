bf87bcc29184fbd0e32f506d689ab82f48d6a321
elpatron68 <elpatron@cepheus.uberspace.de>
2019-05-02 17:10:45 +0200

    OpenAudible

---
+++ openaudible/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/openaudible/openaudible/releases/download/v1.5.0/OpenAudible_win_1.5.0.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url64bit      = $url64
+  
+  softwareName  = 'openaudible*'
+
+  checksum64    = '2D52F536972F55C6440BB47B5F3688CD684FE6B2CFC0675387A9B7B024A0B439'
+  checksumType64= 'sha256'
+
+  silentArgs    = "-q -overwrite"
+  validExitCodes= @(0)
+  
+}
+
+Install-ChocolateyPackage @packageArgs
