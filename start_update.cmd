30839a72dca79e2bd7ee937159758ed105698f88
elpatron68 <elpatron@cepheus.uberspace.de>
2019-05-16 21:38:41 +0200

    Make cmd more verbose

--- start_update.cmd
+++ start_update.cmd
@@ -2,18 +2,22 @@
 cd c:\Users\mbusc\Documents\projects\my-chocolatey-packages
 
 rem Openaudible
+echo Checking openaudible...
 python autoupdate\update_openaudible.py > c:\temp\choco-autoupdate-update.log
 echo. >> c:\temp\choco-autoupdate-update.log
 
 rem Minio Client
+echo Checking Minio Client
 python autoupdate\update_minio-client.py >> c:\temp\choco-autoupdate-update.log
 echo. >> c:\temp\choco-autoupdate-update.log
 
 rem Minio Server
+echo Checking Minio Server
 python autoupdate\update_minio-server.py >> c:\temp\choco-autoupdate-update.log
 echo. >> c:\temp\choco-autoupdate-update.log
 
 rem Gajim
+echo Checking Gajim
 python autoupdate\update_gajim.py >> c:\temp\choco-autoupdate-update.log
 echo. >> c:\temp\choco-autoupdate-update.log
 
