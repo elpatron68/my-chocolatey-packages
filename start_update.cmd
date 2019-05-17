717afb459e2bc0ef8f4ad55c9e96382c72057eec
elpatron68 <elpatron@cepheus.uberspace.de>
2019-05-17 10:48:38 +0200

    Add lanconfig, lanmonitor

--- start_update.cmd
+++ start_update.cmd
@@ -21,5 +21,15 @@ echo Checking Gajim
 python autoupdate\update_gajim.py >> c:\temp\choco-autoupdate-update.log
 echo. >> c:\temp\choco-autoupdate-update.log
 
+rem LANconfig
+echo Checking LANconfig
+python autoupdate\update_lanconfig.py >> c:\temp\choco-autoupdate-update.log
+echo. >> c:\temp\choco-autoupdate-update.log
+
+rem LANmonitor
+echo Checking LANmonitor
+python autoupdate\update_lanmonitor.py >> c:\temp\choco-autoupdate-update.log
+echo. >> c:\temp\choco-autoupdate-update.log
+
 type c:\temp\choco-autoupdate-update.log | telegram-send --stdin
 del c:\temp\choco-autoupdate-update.log
