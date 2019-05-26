9914e96f3c311400ddd2301abdcb637fca194624
elpatron68 <elpatron@cepheus.uberspace.de>
2019-05-26 16:50:09 +0200

    Remove outer brackets

--- start_update.cmd
+++ start_update.cmd
@@ -2,13 +2,13 @@
 cd c:\Users\mbusc\Documents\projects\my-chocolatey-packages
 
 set packages=openaudible minio-client minio-server gajim lanconfig lanmonitor streamwriter usbdlm
-(for %%a in (%packages%) do ( 
+for %%a in (%packages%) do ( 
     echo Checking %%a for update...
     python autoupdate\update_%%a.py >> c:\temp\choco-autoupdate-update.log
     IF %ERRORLEVEL% EQU 0 (echo No update available) ELSE (echo Package updated and commited)
     echo.
     echo. >> c:\temp\choco-autoupdate-update.log
-))
+)
 
 type c:\temp\choco-autoupdate-update.log | telegram-send --stdin
 echo Sent Telegram message
