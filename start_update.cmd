02deac50d9c787883c908a29b91fc08bd1841add
elpatron68 <elpatron@cepheus.uberspace.de>
2019-05-26 15:57:59 +0200

    echo.

--- start_update.cmd
+++ start_update.cmd
@@ -29,28 +29,28 @@ rem LANconfig
 echo Checking LANconfig for update
 python autoupdate\update_lanconfig.py >> c:\temp\choco-autoupdate-update.log
 IF %ERRORLEVEL% EQU 0 (echo No update available) ELSE (echo Package updated and commited)
-echo .
+echo.
 echo. >> c:\temp\choco-autoupdate-update.log
 
 rem LANmonitor
 echo Checking LANmonitor for update
 python autoupdate\update_lanmonitor.py >> c:\temp\choco-autoupdate-update.log
 IF %ERRORLEVEL% EQU 0 (echo No update available) ELSE (echo Package updated and commited)
-echo .
+echo.
 echo. >> c:\temp\choco-autoupdate-update.log
 
 rem Streamwriter
 echo Checking Streamwriter for update
 python autoupdate\update_streamwriter.py >> c:\temp\choco-autoupdate-update.log
 IF %ERRORLEVEL% EQU 0 (echo No update available) ELSE (echo Package updated and commited)
-echo .
+echo.
 echo. >> c:\temp\choco-autoupdate-update.log
 
 rem USBDLM
 echo Checking USBDLM for update
 python autoupdate\update_usbdlm.py >> c:\temp\choco-autoupdate-update.log
 IF %ERRORLEVEL% EQU 0 (echo No update available) ELSE (echo Package updated and commited)
-echo .
+echo.
 echo. >> c:\temp\choco-autoupdate-update.log
 
 type c:\temp\choco-autoupdate-update.log | telegram-send --stdin
