5a2ab60f843b1af77305dab6c2bc9fd8522573d0
elpatron68 <elpatron@cepheus.uberspace.de>
2019-08-29 19:09:27 +0200

    ".\minio-server automatic update"

--- minio-server/tools/chocolateyinstall.ps1
+++ minio-server/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $url64      = 'https://dl.minio.io/server/minio/release/windows-amd64/minio.exe'
   FileFullPath    = "$toolsDir\minio.exe"	
   softwareName    = 'minio-server*'	
 
-   checksum64    = 'e057fa0163777ea8652aa86cb372d294c1d8c82b95845c8b75a0fc3b2861b4a9'	
+   checksum64    = 'c51a426cab791f0c09a8f43aff768cfad1ab25ddccaee2552e8b1f7812296ce3'	
   checksumType64  = 'sha256'	
 }	
 Get-ChocolateyWebFile @packageArgs 
