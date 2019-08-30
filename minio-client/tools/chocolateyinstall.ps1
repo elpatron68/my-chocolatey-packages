5d0fe79f203408da2076cde354131ad3e9d73116
9numbernine9 <numbernine.github@complexnumber.net>
2019-08-30 07:55:54 -0400

    Adjust name of install client executable.

--- minio-client/tools/chocolateyinstall.ps1
+++ minio-client/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   unzipLocation   = $toolsDir
   fileType        = 'EXE'
   url64bit        = $url64
-  FileFullPath    = "$toolsDir\minio.exe"
-  softwareName    = 'minio-server*'
+  FileFullPath    = "$toolsDir\mc.exe"
+  softwareName    = 'minio-client*'
 
   checksum64    = '3beb2b6e9ecb5a0ddaeb5c88b02ee34ce7c1a28c5380cd391ec93f162bb091ca'
   checksumType64  = 'sha256'
