cc0a9b1194c3bd10cfb04f1ecd044bc5c7106132
elpatron68 <elpatron@cepheus.uberspace.de>
2020-04-18 10:30:09 +0200

    ".\openaudible automatic update"

--- openaudible/tools/chocolateyinstall.ps1
+++ openaudible/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/openaudible/openaudible/releases/download/v2.0/OpenAudible_2.0_win.exe'
+$url        = 'https://github.com/openaudible/openaudible/releases/download/v2.0.1/OpenAudible_2.0.1_win.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   
   softwareName  = 'openaudible*'
 
-  checksum      = 'f30a8f90d7fbdc3424bafa747ebd93c05d35952a173470d8fe17dfe2c5c6a127'
+  checksum      = '4165bf9f64fa807ccb36e3fa30c85f155a9161ba1aa6e9a7b8ad89c93e569db4'
   checksumType  = 'sha256'
 
   silentArgs    = "-q -overwrite"
