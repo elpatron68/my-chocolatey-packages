4c2d0e1476404742c30987a69d3616d18fa653e0
elpatron68 <elpatron@cepheus.uberspace.de>
2020-12-18 08:04:43 +0100

    ".\minio-client automatic update"

--- minio-client/tools/chocolateyinstall.ps1
+++ minio-client/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   FileFullPath    = "$toolsDir\mc.exe"
   softwareName    = 'minio-client*'
 
-  checksum64    = 'dfac15e99b8279dd1a83a33651439ed30e624f257c2053bee16c9701fd03811b'
+  checksum64    = 'f7ae6d6c0d0ec7b95f0fb0cfc8651a647d4c0d5f769488a0e9675ef0ab8e93d9'
   checksumType64  = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
