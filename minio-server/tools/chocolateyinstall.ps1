a0131d1ca1afe681aae5170374362747efd1a273
elpatron68 <elpatron@cepheus.uberspace.de>
2021-04-01 13:23:39 +0200

    Minor Changes

--- minio-server/tools/chocolateyinstall.ps1
+++ minio-server/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"	
 $url64      = 'https://dl.min.io/server/minio/release/windows-amd64/minio.exe'	
 
- $packageArgs = @{	
+$packageArgs = @{	
   packageName     = $env:ChocolateyPackageName	
   unzipLocation   = $toolsDir	
   fileType        = 'EXE'	
@@ -10,7 +10,7 @@ $url64      = 'https://dl.min.io/server/minio/release/windows-amd64/minio.exe'
   FileFullPath    = "$toolsDir\minio.exe"	
   softwareName    = 'minio-server*'	
 
-   checksum64    = '0fed4c11a3feaa7f9ff7722ad60434b8299e41eeb31c456fbecb19f0cdb02bcc'	
+  checksum64    = '0fed4c11a3feaa7f9ff7722ad60434b8299e41eeb31c456fbecb19f0cdb02bcc'	
   checksumType64  = 'sha256'	
 }	
 Get-ChocolateyWebFile @packageArgs 
