6f4b5dcc236214d5db651397ee019d70dc700d86
elpatron68 <elpatron@cepheus.uberspace.de>
2021-04-22 19:51:31 +0200

    ".\minio-server automatic update"

--- minio-server/tools/chocolateyinstall.ps1
+++ minio-server/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   FileFullPath    = "$toolsDir\minio.exe"	
   softwareName    = 'minio-server*'	
 
-  checksum64    = 'def4d57244dcb953b0e7cc4e03104dd62772612c438fcfb1847039b45b85317b'	
+  checksum64    = '8157de6c77c01deb4a9ad24d84f47c5e407eb5a5622b0e54c2bb3d7ede67a3ad'	
   checksumType64  = 'sha256'	
 }	
 Get-ChocolateyWebFile @packageArgs 
