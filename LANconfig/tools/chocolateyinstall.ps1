d0dcfe1da1eb4846170f238b9f94c8d5d0eb2e23
elpatron68 <elpatron@cepheus.uberspace.de>
2021-07-17 10:28:00 +0200

    ".\LANconfig automatic update"

--- LANconfig/tools/chocolateyinstall.ps1
+++ LANconfig/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.lancom-systems.de/fileadmin/download/LANtools/LANconfig-10.42.0042-RU3.exe'
+$url        = 'https://www.lancom-systems.de/fileadmin/download/LANtools/LANconfig-10.50.0019-Rel.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   
   softwareName  = 'LANconfig*'
 
-  checksum      = '413a08a5ed56c7c7879665fe20526c03e8baed5e22f1f3ae563c62fe1355b9b9'
+  checksum      = 'bc0176cdfe69f5e48f51d07190dc6a183c8edc0a1bb3ec379f6aa826e6e2faf5'
   checksumType  = 'sha256'
   
   silentArgs    = "/params:/silent"
