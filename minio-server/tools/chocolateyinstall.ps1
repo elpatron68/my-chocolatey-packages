31b5f440c01803876bc8ab351f6d86bbec8ef2d6
elpatron68 <elpatron@cepheus.uberspace.de>
2021-11-09 08:10:43 +0100

    ".\minio-server automatic update"

--- minio-server/tools/chocolateyinstall.ps1
+++ minio-server/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop';	
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"	
-$url64      = 'https://dl.min.io/server/minio/release/windows-amd64/archive/minio.RELEASE.2021-11-05T09-16-26Z'	
+$url64      = 'https://dl.min.io/server/minio/release/windows-amd64/archive/minio.RELEASE.2021-11-09T03-21-45Z'	
 
 $packageArgs = @{	
   packageName     = $env:ChocolateyPackageName	
@@ -10,7 +10,7 @@ $packageArgs = @{
   FileFullPath    = "$toolsDir\minio.exe"	
   softwareName    = 'minio-server*'	
 
-  checksum64    = '19fb987c06304c242a46a871e85451d215456ca15622aa8dba2e8d40d6ae1f9f'	
+  checksum64    = 'c8c7c1e886416e2fb03d149a2fdce541a79ffde6d9b5f4a66bf41a4de1bc38a5'	
   checksumType64  = 'sha256'	
 }	
 # Get-ChocolateyWebFile @packageArgs 
