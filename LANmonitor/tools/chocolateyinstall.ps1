f54187bc17d4b4f05ba071bbe577a7fdd64af1d1
elpatron68 <elpatron@cepheus.uberspace.de>
2021-12-03 09:08:00 +0100

    ".\LANmonitor automatic update"

--- LANmonitor/tools/chocolateyinstall.ps1
+++ LANmonitor/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.lancom-systems.de/fileadmin/download/LANtools/LANmonitor-10.50.0016-RU2.exe'
+$url        = 'https://www.lancom-systems.de/fileadmin/download/LANtools/LANmonitor-10.50.0019-RU3.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'LANmonitor*'
 
-  checksum      = '155fd75616eb6d00bd46c84338b1c0188dd778b73224124193b96c109b7d48a8'
+  checksum      = '2d340b860b525325c3f7078bb674e2d5e5662a6cccddf57fade9c4982766e6b6'
   checksumType  = 'sha256'
 
   silentArgs    = "/params:/silent"
