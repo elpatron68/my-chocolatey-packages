d5ba0d2066344ebc27fab68fdf007eea7f10420a
elpatron68 <elpatron@cepheus.uberspace.de>
2022-05-27 09:30:56 +0200

    Fix Gajim URL

--- gajim/tools/chocolateyinstall.ps1
+++ gajim/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url      = 'https://gajim.org/downloads/1.4/Gajim-1.4.1-64bit.exe'
+$url64 = 'https://gajim.org/downloads/1.4/Gajim-1.4.2-64bit.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
+  url           = $url64
 
   softwareName  = 'gajim*'
 
-  checksum    = 'a91fe698279ff0fb6548228b04ca4c0d26cc4c598c4bb8b397551e32f0b9b402'
-  checksumType= 'sha256'
+  checksum64    = 'F68DDC70E2E9F5CD4BE9B4D953E1D20CF4489312C11C49B81601A1C91EF85BF8'
+  checksumType  = 'sha256'
 
   silentArgs    = "/S"
   validExitCodes= @(0)
