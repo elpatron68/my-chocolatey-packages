11b272a7747d376007b4a21728044a8349536a16
elpatron68 <elpatron@cepheus.uberspace.de>
2022-07-08 11:19:05 +0200

    ".\gajim automatic update"

--- gajim/tools/chocolateyinstall.ps1
+++ gajim/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url64      = 'https://gajim.org/downloads/1.4/Gajim-1.4.5-64bit.exe'
+$url64      = 'https://gajim.org/downloads/1.4/Gajim-1.4.6-64bit.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
 
   softwareName  = 'gajim*'
 
-  checksum64    = '1dcfd307ebafcd795a52ef8384c939934fd711a640f10620f5f59d4154164d2d'
+  checksum64    = '8e2f1533d5612778ba43071c2d5d33fbd32ebb89d1123be54a83a45132f1b5bc'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"
