d327e3a802f945fceebdd60965292c61b41db9b9
elpatron68 <elpatron@cepheus.uberspace.de>
2022-07-12 11:21:18 +0200

    Quiet delete

--- start_update.cmd
+++ start_update.cmd
@@ -1,7 +1,7 @@
 @echo off
 cd C:\Users\mbusc\source\repos\my-chocolatey-packages
 set LOGFILE=%TEMP%\choco-autoupdate-update.log
-del /q %LOGFILE%
+del /q %LOGFILE% >nul 2>&1
 echo Git pull...
 echo Git pull: > %LOGFILE%
 git pull origin master >> %LOGFILE%
@@ -9,7 +9,7 @@ echo.
 echo. >> %LOGFILE%
 set packages=openaudible gajim lanconfig lanmonitor streamwriter usbdlm explorergenie
 for %%a in (%packages%) do (
-    del /q %%a\*.nupkg
+    del /q %%a\*.nupkg >nul 2>&1
     echo Checking %%a for update...
     python autoupdate\update_%%a.py >> %LOGFILE%
     rem IF %ERRORLEVEL% EQU 0 (echo No update available) ELSE (echo Package updated and commited)
