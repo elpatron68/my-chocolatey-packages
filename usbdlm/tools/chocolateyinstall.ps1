cffb9acdb5d99e20912fd77b5c65275b7f68be99
elpatron68 <elpatron@cepheus.uberspace.de>
2022-07-25 14:09:33 +0200

    ".\usbdlm automatic update"

--- usbdlm/tools/chocolateyinstall.ps1
+++ usbdlm/tools/chocolateyinstall.ps1
@@ -13,9 +13,9 @@ $packageArgs = @{
 
   softwareName  = 'usbdlm*'
 
-  checksum      = '2af65646ef5b4cb99652cac9dde056b50861c39b4830d52defd30ceecbfdfbb4'
+  checksum      = '88c78609e544744301d746a33ba6eba0631ce40196ecc76ca1ed87844743f5e0'
   checksumType  = 'sha256'
-  checksum64    = '63108a45765b30e277f45cf074ec88635d992bb6ebadc5fb227338c5f767f976'
+  checksum64    = '6a7fd7e7385eb7aa44186c65c705682176c4cda29eda4fddb4d69985288d5f28'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
