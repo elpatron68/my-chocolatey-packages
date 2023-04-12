38898138fdb902918953213666bd97a37477a99d
elpatron68 <elpatron@cepheus.uberspace.de>
2023-04-12 08:09:35 +0200

    Remove openaudible from batch run

--- start_update.cmd
+++ start_update.cmd
@@ -7,7 +7,7 @@ echo Git pull: > %LOGFILE%
 git pull origin master >> %LOGFILE%
 echo.
 echo. >> %LOGFILE%
-set packages=openaudible gajim lanconfig lanmonitor streamwriter usbdlm explorergenie
+set packages=gajim lanconfig lanmonitor streamwriter usbdlm explorergenie
 for %%a in (%packages%) do (
     del /q %%a\*.nupkg >nul 2>&1
     echo Checking %%a for update...
