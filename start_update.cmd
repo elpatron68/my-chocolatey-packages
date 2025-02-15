70f05cf6063768255356489e6becb82aa0c0d16e
elpatron <m.busche@gmail.com>
2025-02-15 10:41:55 +0100

    Add avpn to routine

--- start_update.cmd
+++ start_update.cmd
@@ -10,7 +10,7 @@ echo Git pull: > %LOGFILE%
 git pull origin master >> %LOGFILE%
 echo.
 echo. >> %LOGFILE%
-set packages=gajim lanconfig lanmonitor streamwriter usbdlm explorergenie
+set packages=gajim lanconfig lanmonitor streamwriter usbdlm avpn
 for %%a in (%packages%) do (
     del /q %%a\*.nupkg >nul 2>&1
     echo Checking %%a for update...
