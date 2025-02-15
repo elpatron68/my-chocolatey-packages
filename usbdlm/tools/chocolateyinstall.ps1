5da142303561f5199b16791ee6366994e35a6f9e
elpatron <m.busche@gmail.com>
2025-02-15 09:30:11 +0100

    USBDLM

--- usbdlm/tools/chocolateyinstall.ps1
+++ usbdlm/tools/chocolateyinstall.ps1
@@ -1,23 +1,19 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.uwe-sieber.de/files/usbdlm.msi'
-$url64      = 'https://www.uwe-sieber.de/files/usbdlm_x64.msi'
+$url        = 'https://www.uwe-sieber.de/files/usbdlm_x64.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'usbdlm*'
 
-  checksum      = 'a13769377fc363f2feeb2afb6a8107753981445c25f0fa8d6a99e56e2c54a2ee'
+  checksum      = '5F6D01DDE7E259354F36DD134C6836BB9BC7E4219CCF089F02DE5AD7B670C617'
   checksumType  = 'sha256'
-  checksum64    = '99700ae2ec065a3674df95e49c0a6b4aa363a0c55245befb239146362b6f12b8'
-  checksumType64= 'sha256'
-
+  
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0,3010)
 }
