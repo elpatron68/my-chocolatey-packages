e1b56dfb1fd73949ee5a3dea633e942c11304acb
elpatron <m.busche@gmail.com>
2025-02-15 14:20:50 +0100

    Remove rebootblocker, update streamwriter

--- streamwriter/tools/chocolateyinstall.ps1
+++ streamwriter/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://streamwriter.org/en/downloads/2/'
+$url        = 'https://streamwriter.org/en/downloads/streamwriter_setup-x86_64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'streamwriter*'
 
-  checksum      = 'e2618fd080bf37ac41e57c6be6a07d8ee615a45682768442c8b27c92ebeda1d3'
+  checksum      = 'BE0A5A6636BB52DAD4D3685BE47EF919C8F667582ABCBD7421332FFDF345ED31'
   checksumType  = 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
