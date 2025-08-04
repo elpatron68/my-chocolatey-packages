1d3a68d300cb2d0145b0dc6edfe886a436279670
elpatron <m.busche@gmail.com>
2025-08-04 11:05:10 +0200

    Fix checksum problem

--- gajim/tools/chocolateyinstall.ps1
+++ gajim/tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url64      = 'https://gajim.org/downloads/2.3/Gajim-2.3.3-64bit.exe'
+$url      = 'https://gajim.org/downloads/2.3/Gajim-2.3.3-64bit.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url64
+  url           = $url
 
   softwareName  = 'gajim*'
 
-  checksum64    = '8bf67474da9525806547ff809c196b12578b2b0a77e8705fad19c5bdb1e602fb'
+  checksum    = '8bf67474da9525806547ff809c196b12578b2b0a77e8705fad19c5bdb1e602fb'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"
