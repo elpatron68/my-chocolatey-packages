906fe1a23761415aa11d42ccf3f0d5a2969bde21
elpatron <m.busche@gmail.com>
2025-09-18 11:20:18 +0200

    ".\gajim automatic update"

--- gajim/tools/chocolateyinstall.ps1
+++ gajim/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url        = 'https://gajim.org/downloads/2.3/Gajim-2.3.4-64bit.exe'
+$url        = 'https://gajim.org/downloads/2.3/Gajim-2.3.5-64bit.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
 
   softwareName  = 'gajim*'
 
-  checksum      = 'f1f169d75c2eb10c91c8c61f26e16e7dcaf8e8a66681bbac7af3a424451f3911'
+  checksum      = 'f45039ca55a0404dd677f5fd9d4cc11960b729bdd546b0e0d251fa0fd0077883'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"
