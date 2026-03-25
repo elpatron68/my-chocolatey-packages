051a0940691b6a3c0f056855282f7220c1ccad89
elpatron <m.busche@gmail.com>
2026-03-25 11:19:22 +0100

    ".\LANCOM-VPN-Client automatic update"

--- LANCOM-VPN-Client/tools/chocolateyinstall.ps1
+++ LANCOM-VPN-Client/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://my.lancom-systems.de/download/LC-VPN-Client/LC-Advanced-VPN-Client-Win-624-Rel-x86-64.exe'
+$url        = 'https://my.lancom-systems.de/download/LC-VPN-Client/LC-Advanced-VPN-Client-Win-625-Rel-x86-64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   
   softwareName  = 'LANCOM Trusted Access Client*'
 
-  checksum      = '3b3cf3b41d0185425652eefe883694dd6a2c2b087ac50a3d72672cb6c748819b'
+  checksum      = '3be63daaf76442dc00d8ab77bac7e11b0c0786b0a91e5be7c1e51d71f189933f'
   checksumType  = 'sha256'
   
   silentArgs    = '/s /l1031< /v"/qn /norestart"'
